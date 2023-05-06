bb108379dd341463bf66713e02b0a3a37c38da6a
Ray Vafa <vafatabar@gmail.com>
2023-05-06 19:17:32 +1000

    basic button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import Button from "./components/Button";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button success primary rounded outline>Click me!!</Button>
+      </div>
+      <div>
+        <Button danger outline>Buy Now!</Button>
+      </div>
+      <div>
+        <Button warning>See Deal!</Button>
+      </div>
+      <div>
+        <Button secondary outline>Hide Ads!</Button>
+      </div>
+      <div>
+        <Button secondary rounded>Something!</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
