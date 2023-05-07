f9fa5aa93b343f2bc01c5f53af6b920600689c6d
Ray Vafa <vafatabar@gmail.com>
2023-05-07 19:11:06 +1000

    dropdown element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import AccordionPage from "./pages/AccordionPage";
+import {useState} from "react";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  return <AccordionPage />;
+  const [selection, setSelection] = useState(null);
+
+  const handleSelection = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
+  ];
+
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelection} />
+    </div>
+  );
 }
 
 export default App;
