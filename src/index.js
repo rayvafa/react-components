c8d12aa39ec805d0cade8ecd9a2444b1aaba0b5f
Ray Vafa <vafatabar@gmail.com>
2023-05-09 18:07:49 +1000

    adding navigation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {NavigationProvider} from "./context/navigation";
 import App from './App';
 
 const element = document.getElementById('root');
 const root = ReactDOM.createRoot(element);
 
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
